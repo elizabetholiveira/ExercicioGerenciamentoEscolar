331be4758be46bd921d05de92d699fe6814cbe70
elizabetholiveira <elizabeth.oliveira@zup.com.br>
2023-08-23 17:02:31 -0300

    segundo commit mockmvc

--- gerenciamentoEscolar/src/test/java/com/exercicio/gerenciamentoEscolar/test/AlunosControllerTest.java
+++ gerenciamentoEscolar/src/test/java/com/exercicio/gerenciamentoEscolar/test/AlunosControllerTest.java
@@ -1,27 +1,48 @@
 package com.exercicio.gerenciamentoEscolar.test;
 
 import com.exercicio.gerenciamentoEscolar.controller.AlunosController;
+import com.exercicio.gerenciamentoEscolar.model.AlunosModel;
+import com.exercicio.gerenciamentoEscolar.service.AlunosService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringJUnitConfig
+//@SpringJUnitConfig
 @WebMvcTest(AlunosController.class)
 //@AutoConfigureMockMvc
 //@SpringBootTest
 public class AlunosControllerTest {
 
+    @MockBean
+    AlunosService alunosService;
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    public void testCadastrar() throws Exception {
+        AlunosModel novoAluno = new AlunosModel(001L, "Maria", "25", "maria@gmail.com");
+        when(alunosService.cadastrar(novoAluno)).thenReturn(novoAluno);
+        mockMvc.perform(post("/api/alunos"))
+                //.contentType(MediaType.APPLICATION_JSON)
+                //.content(new ObjectMapper().writeValueAsString(novoAluno))
+                .andExpect(status().isOk());
+    }
 
     @Test
     public void testListarTodos() throws Exception{
