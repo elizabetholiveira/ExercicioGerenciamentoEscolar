75f7bc566078491627db6746b6215df1788b9be3
elizabetholiveira <elizabeth.oliveira@zup.com.br>
2023-08-23 17:46:07 -0300

    terceiro commit mockmvc

--- gerenciamentoEscolar/src/test/java/com/exercicio/gerenciamentoEscolar/test/AlunosControllerTest.java
+++ gerenciamentoEscolar/src/test/java/com/exercicio/gerenciamentoEscolar/test/AlunosControllerTest.java
@@ -16,27 +16,35 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@SpringJUnitConfig
-@WebMvcTest(AlunosController.class)
-//@AutoConfigureMockMvc
-//@SpringBootTest
+//@WebMvcTest(AlunosController.class)
+@AutoConfigureMockMvc
+@SpringBootTest
 public class AlunosControllerTest {
 
     @MockBean
-    AlunosService alunosService;
+    private AlunosService alunosService;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    void  shouldCreateMockMvc(){
+        assertNotNull(mockMvc);
+    }
+
+    @Test
     public void testCadastrar() throws Exception {
-        AlunosModel novoAluno = new AlunosModel(001L, "Maria", "25", "maria@gmail.com");
+        AlunosModel novoAluno = new AlunosModel(001L, "Maria", "25", "maria@email.com");
         when(alunosService.cadastrar(novoAluno)).thenReturn(novoAluno);
         mockMvc.perform(post("/api/alunos"))
                 //.contentType(MediaType.APPLICATION_JSON)
@@ -44,12 +52,23 @@ public class AlunosControllerTest {
                 .andExpect(status().isOk());
     }
 
+//    @Test
+//    public void testListarTodos() throws Exception{
+//        mockMvc.perform(get("/api/alunos"))
+//                .andExpect(status().isOk())
+//                .andExpect(view().name("api/alunos/list"))
+//                .andExpect(model().attributeExists("api/alunos"));
+//    }
+
     @Test
-    public void testListarTodos() throws Exception{
-        mockMvc.perform(get("/api/alunos"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("api/alunos/list"))
-                .andExpect(model().attributeExists("api/alunos"));
+    public void testListarTodos() throws Exception {
+        when(alunosService.listarTodos())
+                .thenReturn(List.of(new AlunosModel(001L, "Maria", "25", "maria@email.com")));
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get("/api/alunos"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
